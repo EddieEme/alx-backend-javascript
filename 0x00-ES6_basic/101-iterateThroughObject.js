8cf984858d9ad632e85a7d99250b34a9d8d7b546
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-10-02 23:55:38 +0100

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,12 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  let isFirst = true;
+  for (const item of reportWithIterator) {
+    if (!isFirst) {
+      result += ' | ';
+    }
+    result += item;
+    isFirst = false;
+  }
+  return result;
+}
