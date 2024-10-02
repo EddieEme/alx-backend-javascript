12511a01662a252c91685acd1b18468ab1d1ab62
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-10-02 20:23:02 +0100

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,15 +1,11 @@
 export default function createIteratorObject(report) {
-    const allEmployees = report.allEmployees
-
-    
-    function* employeeIterator() {
-        for (const department of Object.values(allEmployees)) {
-        for (const employee of department) {
-            yield employee;
-        }
+  const allEmployees = report.allEmployees;
+  function* employeeIterator() {
+    for (const department of Object.values(allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
     }
-    
-    }
-   
-   return  employeeIterator()
+  }
+  return employeeIterator();
 }
